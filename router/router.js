dedffc57c23be1e7a6599afbdbc44ecb6d7297d9
Emam546 <emam54637@gmail.com>
2023-03-13 02:51:38 +0200

    add package.json file

--- router/router.js
+++ router/router.js
@@ -1,33 +1,34 @@
-const url = require('url');
-const express = require('express');
-const router = express.Router()
-const rateLimit = require('express-rate-limit');
-const needle = require('needle');
-const apicache = require('apicache');
-const { API_BASE_URL, API_KEY_NAME, API_KEY } = process.env
+const url = require("url");
+const express = require("express");
+const router = express.Router();
+const rateLimit = require("express-rate-limit");
+const needle = require("needle");
+const apicache = require("apicache");
+const { API_BASE_URL, API_KEY_NAME, API_KEY } = process.env;
 
 const Limiter = rateLimit({
     windowMs: 10 * 60 * 1000,
-    max: 5
-})
-router.use(Limiter)
-const cache = apicache.middleware
+    max: 5,
+});
+router.use(Limiter);
+const cache = apicache.middleware;
 
+const onlyStatus200 = (req, res) => res.statusCode === 200;
 
-router.get("/", cache("2 minutes"), async (req, res) => {
+const cacheSuccesses = cache("5 minutes", onlyStatus200);
+router.get("/", cacheSuccesses, async (req, res) => {
     try {
         const params = new URLSearchParams({
             appid: API_KEY,
-            ...req.query
-        })
+            ...req.query,
+        });
         if (process.env.NODE_ENV !== "production")
             console.log(`${API_BASE_URL}${params}`);
-        const respond = await needle("get", `${API_BASE_URL}${params}`,)
-        res.json(respond.body)
+        const respond = await needle("get", `${API_BASE_URL}${params}`);
+        res.json(respond.body);
     } catch (error) {
         console.error(error);
-        res.status(500).json({ error })
-
+        res.status(500).json({ error });
     }
-})
-module.exports = router
+});
+module.exports = router;
